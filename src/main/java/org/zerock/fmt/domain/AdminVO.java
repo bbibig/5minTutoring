171a4cc0972171a936d031affc77807ecf1613b7
SUIN <suyin7636@gmail.com>
2022-08-16 23:33:47 +0900

    createVOtable

---
+++ src/main/java/org/zerock/fmt/domain/AdminVO.java
@@ -0,0 +1,13 @@
+package org.zerock.fmt.domain;
+
+import lombok.Value;
+
+@Value
+public class AdminVO {
+
+	private String ad_id;
+	private String ad_name;
+	private String ad_dep_name;
+	private String ad_email;
+
+} // end class
