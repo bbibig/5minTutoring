62bce384763173b7cda401d44f4dc4af8632745b
JiHyeon <bjh1540@naver.com>
2022-08-18 01:54:47 +0900

    add mapper file

--- src/main/java/org/zerock/fmt/domain/TutorPageVO.java
+++ src/main/java/org/zerock/fmt/domain/TutorPageVO.java
@@ -8,7 +8,7 @@ public class TutorPageVO {
 	private Integer tp_number;
 	private String user_email;
 	private String tp_career;
-	private String tp_title;
+	private String tp_title;	
 	private String tp_comment;
 	private String tp_accu_answer;	// 누적 답변
 	private Double tp_average;
